e863c4296adc1aefe0b3fcf806b0fb58c85d712d
Courtney Moran <courtney@DN525fcp.SUNet>
2019-10-31 15:41:56 -0700

    linear models

--- linear.py
+++ linear.py
@@ -24,3 +24,25 @@ def LinReg(x,y):
   plt.scatter(x_test, y_test,  color='black')
   plt.plot(x_test, y_pred, color='blue', linewidth=3)
   plt.show()
+
+
+ def RANSAC(x,y):
+
+   #split data into train & test
+   x_train,y_train,x_test,y_test = util.split_data(x,y)
+
+   #fit linear regression model
+   linreg = linear_model.RANSACRegressor()
+   linreg.fit(x_train,y_train)
+
+   #make predictions on test data
+   y_pred = linreg.predict(x_test)
+
+   #evaluate performance
+   print("Mean squared error: %.2f" %mean_squared_error(y_test,y_pred))
+   print('Variance score: %.2f' % r2_score(y_test, y_pred))
+
+   #plot test data & predictions on test data
+   plt.scatter(x_test, y_test,  color='black')
+   plt.plot(x_test, y_pred, color='blue', linewidth=3)
+   plt.show()
