c837f5d254b639b6841a3806a1dac331d743efed
Courtney Moran <courtney@DN0a1e8d5f.SUNet>
2019-10-31 14:42:30 -0700

    linear models

--- linear.py
+++ linear.py
@@ -1,6 +1,6 @@
 from sklearn import linear_model
 import numpy as np
-import matplotlib as plt
+import matplotlib.PyPlot as plt
 from sklearn.metrics import mean_squared_error, r2_score
 import util
 
@@ -21,6 +21,6 @@ def LinReg(x,y):
   print('Variance score: %.2f' % r2_score(y_test, y_pred))
 
   #plot test data & predictions on test data
-  plt.plot(x_test, y_test,  color='black')
+  plt.scatter(x_test, y_test,  color='black')
   plt.plot(x_test, y_pred, color='blue', linewidth=3)
   plt.show()
