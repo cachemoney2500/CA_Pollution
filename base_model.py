408041b799d9546c37ecc902b46fadf427c81b38
Richard Li <richardli@DN0a1e47cc.SUNet>
2019-11-20 04:55:27 -0800

    added bearings as a feature and visualization

--- base_model.py
+++ base_model.py
@@ -4,8 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from geopy.distance import geodesic
+from geographiclib.geodesic import Geodesic as geodesic2
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
+
+import pdb
 
 
 #return a set of all measurement site numbers in a county
@@ -26,22 +29,20 @@ def add_points(start,end,fire_lat,fire_long):
     CO_fire = CO_fire[CO_fire["Longitude"].between(fire_long-2,fire_long+2)]
 
     #store latitude & longitude of each site & the peak CO reading from the duration of the fire
-    distances = []
-    peaks = []
+    distances, bearings, peaks = [], [], []
     sites = extract_sites(CO_fire)
-    for site in sites:
+    for i, site in enumerate(sites):
         site_data = CO_fire[CO_fire["Site Num"]==site]
         lat = site_data["Latitude"].tolist()[0]
         long = site_data["Longitude"].tolist()[0]
         peaks.append(np.nanmax(site_data["AQI"]))
         distances.append(geodesic((fire_lat,fire_long),(lat,long)).miles)
+        bearings.append(geodesic2.WGS84.Inverse(fire_lat,fire_long,lat,long)['azi1'])
 
-    return distances,peaks
+    return distances,bearings,peaks
 
-def extract_data(fires):
-    distances = []
-    fire_sizes = []
-    peaks = []
+def extract_data(fires, with_bearings=True):
+    distances, bearings,fire_sizes, peaks = [], [], [], []
     #add data points for each fire
     for index, row in fires.iterrows():
         start = row["Start Date"]
@@ -49,17 +50,21 @@ def extract_data(fires):
         fire_lat = row["Lattitude"]
         fire_long = row["Longitude"]
         fire_size = row["Acres Burned"]
-        d,p = add_points(start,end,fire_lat,fire_long)
+        d,b,p = add_points(start,end,fire_lat,fire_long)
         distances += d
+        bearings += b
         peaks += p
         fire_sizes += [fire_size]*len(d)
 
     #form X and y from data
     #features in X are distance from fire, and size of fire
     #labels are the peak CO reading from the time frame of the fire
-    X = np.zeros((len(distances),2))
+    if with_bearings: X = np.zeros((len(distances),3))
+    else: X = np.zeros((len(distances),2))
+    
     X[:,0] = np.array(distances)
     X[:,1] = np.array(fire_sizes)
+    if with_bearings: X[:,2] = np.array(bearings)
     y = np.array(peaks)
 
     return X,y
@@ -70,42 +75,49 @@ if __name__ == "__main__":
     #read in dataset
     CO = pd.read_csv("CA_COdata_2009to2018.csv")
     fires = pd.read_csv("fires.csv")
-
+    
     #convert date column to dt objects
     CO["Date Local"] =  pd.to_datetime(CO["Date Local"],format='%Y-%m-%d')
+    
 
-
+# for comparing model performance with and without bearings for all fires
+#    for test_fire in fires['Fire Name']:
+#        for bearings in [True, False]:
+    
     #extract inputs & labels from the data set
-    X_train,y_train = extract_data(fires[fires["Fire Name"]!="Station"])
-    X_test,y_test = extract_data(fires[fires["Fire Name"]=="Station"])
+    test_fire = 'Woolsey'
+    X_train,y_train = extract_data(fires[fires["Fire Name"]!=test_fire], with_bearings=True)
+    X_test,y_test = extract_data(fires[fires["Fire Name"]==test_fire], with_bearings=True)
 
     #fit a linear regression model to the data and predict for a single fire
     linReg = linear_model.LinearRegression()
     linReg.fit(X_train,y_train)
     y_pred = linReg.predict(X_test)
-    print("Linear Regression mean squared error: %.2f"
+    print("Linear Regression MSE: %.2f"
       % mean_squared_error(y_test, y_pred))
     print('Linear Regression variance score: %.2f' % r2_score(y_test, y_pred))
-
-
+    
+    
     #fit a RANSAC regression model to the data and predict for a single fire
     ransac = linear_model.RANSACRegressor(random_state=20)
     ransac.fit(X_train,y_train)
     y_pred_r = ransac.predict(X_test)
-    print("RANSAC Regression mean squared error: %.2f"
+    print("RANSAC Regression MSE: %.2f"
       % mean_squared_error(y_test, y_pred_r))
     print('RANSAC Regression variance score: %f' % r2_score(y_test, y_pred_r))
 
     plt.plot(X_test[:,0],y_pred,'r')
-    plt.scatter(X_test[:,0],y_test)
+    plt.scatter(X_test[:,0],y_test, c=X_test[:,2], cmap='viridis')
     plt.xlabel("Distance from Fire")
     plt.ylabel("Peak CO Readings")
-    plt.title("Station Fire Linear Model")
-
+    plt.title("%s Fire Linear Model" %test_fire)
+    plt.colorbar()
+    
     plt.figure(2)
     plt.plot(X_test[:,0],y_pred_r,'r')
-    plt.scatter(X_test[:,0],y_test)
+    plt.scatter(X_test[:,0],y_test, c=X_test[:,2], cmap='viridis')
     plt.xlabel("Distance from Fire")
     plt.ylabel("Peak CO Readings")
-    plt.title("Station Fire RANSAC Model")
+    plt.title("%s Fire RANSAC Model" %test_fire)
+    plt.colorbar()
     plt.show()
